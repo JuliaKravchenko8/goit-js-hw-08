d15ac6f0b61392da667624ddaaaaa0a0b4dfe653
JuliaKravchenko8 <107355879+JuliaKravchenko8@users.noreply.github.com>
2022-11-15 00:32:57 +0200

    update task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,16 +4,15 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Vimeo(iframe);
 
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
 function onPlay({ seconds }) {
-  localStorage.setItem(
-    'videoplayer-current-time',
-    JSON.stringify(data.seconds)
-  );
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(seconds));
 }
 
 player.on('timeupdate', throttle(onPlay, 1000));
-
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
 
 let seconds = JSON.parse(localStorage.getItem('videoplayer-current-time'));
 if (seconds) {
